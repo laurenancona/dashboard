f49eebccbf69e138af9869f4d2cb66cca8a53cc2
Lauren Ancona <lauren.ancona@phila.gov>
2015-07-03 13:02:02 -0400

    no apis

--- lib/websites.rb
+++ lib/websites.rb
@@ -3,7 +3,7 @@ require 'hashie'
 $config = File.open("websites.json") { |f| Hashie::Mash.new(JSON.load(f)) }
 
 # process config file
-for property in $config.websites + $config.apis
+for property in $config.websites #+ $config.apis
   # default id from title
   property.id = property.title.downcase.gsub(/[ .]/, '-') if property.id.nil?
 end
